2ac7bec45ea249ac3148daa3e35d781268f2689a
Yago <104085125+YagoGomez83@users.noreply.github.com>
2024-03-29 15:59:13 -0300

    Campo CC Agregado

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const inputCC = document.querySelector("#cc");
   const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
   const btnReset = document.querySelector('#formulario button[type="reset"]');
@@ -18,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+  inputCC.addEventListener("input", validar);
   formulario.addEventListener("submit", enviarEmail);
   btnReset.addEventListener("click", (e) => {
     e.preventDefault();
@@ -68,7 +70,10 @@ document.addEventListener("DOMContentLoaded", () => {
       comprobarEmail();
       return;
     }
-    if (e.target.id === "email" && !validarEmail(e.target.value)) {
+    if (
+      (e.target.id === "email" || e.target.id === "cc") &&
+      !validarEmail(e.target.value)
+    ) {
       mostrarAlerta("El Email no es valido", e.target.parentElement);
       email[e.target.id] = "";
       comprobarEmail();
